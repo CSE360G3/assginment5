fc65bdafce76c16f51d9d61d793c49a0ce702ec0
CSE360group3 <thnguy17@asu.edu>
2014-10-26 20:35:28 -0700

    Update urls.py

--- image_space/urls.py
+++ image_space/urls.py
@@ -8,5 +8,12 @@ urlpatterns = patterns('',
 
     url(r'^$', include('image_space_app.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^home/$', include('image_space_app.urls')),
+    url(r'^signup/$', 'image_space_app.views.sign_up'),
+    url(r'^login/$', 'image_space_app.views.login_user', name = 'login'),
+    url(r'^register/$', 'image_space_app.views.register', name = 'register'),
+    url(r'^profile/$', 'image_space_app.views.profile', name = 'profile'),
+    url(r'^logout/$', 'image_space_app.views.log_out', name = 'logout'),
+
 
 )
