9821dc1354747a2262acf12e1c406bb9408086c7
CSE360group3 <thnguy17@asu.edu>
2014-11-24 21:48:21 -0700

    Update models.py

--- image_space_app/models.py
+++ image_space_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    picture = models.ImageField(upload_to='profile_images', blank = True)
+
+    def __unicode__(self):
+        return self.user.username
+		
+class UserPhoto0(models.Model):
+    picture = models.ImageField(upload_to='profile_images', blank = False)
+	
